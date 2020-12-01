7fbf2d2f8e55afa634b0fc13d82a6206a638f8a5
rej696 <55083730+rej696@users.noreply.github.com>
2020-12-01 16:49:37 +0000

    create init for flask package, write db handling functions

---
+++ criticle/db.py
@@ -0,0 +1,52 @@
+import sqlite3
+
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
+
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+    
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+
+def init_db():
+    db = get_db()
+
+    # drop exisiting tables using drop_tables.sql script
+    with current_app.open_resource('sql_scripts/drop_tables.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+    
+    # create tables for database from schema.sql
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+    
+    # populate database using populate.sql script
+    with current_app.open_resource('sql_scripts/populate.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables"""
+    init_db()
+    click.echo('Initialised the database')
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
