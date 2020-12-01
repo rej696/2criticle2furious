391b4079b16bf313b75d161c3004aa2e4970c238
rej696 <55083730+rej696@users.noreply.github.com>
2020-12-01 11:30:07 +0000

    added database tables to schema

---
+++ criticle/schema.sql
@@ -0,0 +1,60 @@
+-- Define database tables
+
+-- Images table for referencing user and media profile pictures
+create table images (
+    id integer primary key autoincrement not null,
+    title varchar(255),
+    filepath varchar(255)
+);
+
+-- Media Database Tables
+
+-- Movies table
+create table movies (
+    id integer primary key autoincrement not null,
+    title varchar(255) not null,
+    director varchar(255),
+    genre varchar(255),
+    summary text,
+    image_id integer,
+    foregin key (image_id) references images (id)
+);
+
+-- Books table
+create table books (
+    id integer primary key autoincrement not null,
+    title varchar(255) not null,
+    author varchar(255),
+    genre varchar(255),
+    summary text,
+    image_id integer,
+    foregin key (image_id) references images (id)
+);
+
+-- Categories Database
+create table categories (
+    id integer primary key autoincrement not null,
+    media_type varchar(255) not null -- should reference the title of a media table
+);
+
+-- User Database
+create table users (
+    id integer primary key autoincrement not null,
+    username varchar(255) not null,
+    firstname varchar(255),
+    lastname varchar(255),
+    age integer,
+    image_id integer
+);
+
+-- Review Map Database
+create table reviews (
+    id integer primary key autoincrement not null,
+    category_id integer not null,
+    media_id integer not null,
+    user_id integer not null,
+    body text,
+    rating integer,
+    foreign key (category_id) references categories(id) not null,
+    foreign key (user_id) references users(id) not null
+);
